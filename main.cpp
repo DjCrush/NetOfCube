15d3e554e852431c76e057049bf509173b565cc0
Yevgeny Antonov <yevgeny1978@mail.ru>
2019-02-28 01:52:39 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <SDL.h> // using SDL 1.2.15
+#include <SDL.h> // SDL 1.2.15
 #include <vector>
 using namespace std;
 
@@ -9,43 +9,47 @@ constexpr int BPP = 4;
 constexpr int DEPTH = 32;
 constexpr double PI = 3.1415926535897932384626433832795f;
 
-struct Point
+class Mesh
 {
-	Point(double x, double y, double z) : x{ x }, y{ y }, z{ z } {}
-	double x, y, z;
+public:
+	Mesh(SDL_Surface* screen);
+	void draw();
+private:
+	struct Point
+	{
+		Point(double x = 0, double y = 0, double z = 0) : x{ x }, y{ y }, z{ z } {}
+		double x, y, z;
+	};
+	struct Edge
+	{
+		Edge(Point point1 = { }, Point point2 = { }) : point1{ point1 }, point2{ point2 } {}
+		Point point1, point2;
+	};
+	void rotateVerticesAroundX(double angle);
+	void rotateVerticesAroundY(double angle);
+	void rotateVerticesAroundZ(double angle);
+	void createCubeEdges(); // Сreating cube edges using vertices
+	void drawWireFrame(Uint8 red, Uint8 green, Uint8 blue);
+	void downFaces();
+	void rotatePointAroundX(Point& point1, const Point& point2, double angle);
+	void rotatePointAroundY(Point& point1, const Point& point2, double angle);
+	void rotatePointAroundZ(Point& point1, const Point& point2, double angle);
+	SDL_Surface *screen;
+	int i, i1;
+	vector<Edge> edges;
+	vector<Point> vertices, verticesCopy;
 };
-
-struct Edge
-{
-	Edge(Point point1, Point point2) : point1{ point1 }, point2{ point2 } {}
-	Point point1, point2;
-};
-
 
 void clearScreen(SDL_Surface* screen);
 inline void drawPixel(SDL_Surface *screen, int x, int y, Uint8 red, Uint8 green, Uint8 blue);
 void drawLine(SDL_Surface* screen, int x1, int y1, int x2, int y2, Uint8 red, Uint8 green, Uint8 blue);
 void drawCircle(SDL_Surface* screen, int x0, int y0, int radius, Uint8 red, Uint8 green, Uint8 blue);
 
-void rotateVerticesAroundX(vector<Point>& vertices, double angle);
-void rotateVerticesAroundY(vector<Point>& vertices, double angle);
-void rotateVerticesAroundZ(vector<Point>& vertices, double angle);
-void createCubeVertices(vector<Point>& vertices); // Сreating cube vertices
-vector<Edge> createCubeEdges(const vector<Point>& vertexes); // Сreating cube edges using vertices
-void drawWireFrame(SDL_Surface* screen, const vector<Edge>& edges, Uint8 red, Uint8 green, Uint8 blue);
-void downFaces1(vector<Point>& vertices);
-void downFaces2(vector<Point>& vertices);
-void rotatePointAroundX(Point& point1, const Point& point2, double angle);
-void rotatePointAroundY(Point& point1, const Point& point2, double angle);
-void rotatePointAroundZ(Point& point1, const Point& point2, double angle);
-
 int wmain(int argc, wchar_t *argv[], wchar_t *envp[])
 {
 	SDL_Event event;
-	SDL_Surface *screen;
-	int keypress{}, i{}, i1{};
-	vector<Edge> cube;
-	vector<Point> vertices;
+	SDL_Surface* screen = NULL;
+	int keypress{};
 
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) return 1;
 	if (!(screen = SDL_SetVideoMode(WIDTH, HEIGHT, DEPTH, SDL_HWSURFACE)))
@@ -53,7 +57,7 @@ int wmain(int argc, wchar_t *argv[], wchar_t *envp[])
 		SDL_Quit();
 		return 1;
 	}
-	createCubeVertices(vertices);
+	Mesh cube(screen);
 	while (!keypress)
 	{
 		while (SDL_PollEvent(&event))
@@ -69,33 +73,46 @@ int wmain(int argc, wchar_t *argv[], wchar_t *envp[])
 			}
 		}
 		clearScreen(screen);
-		if (i1 <= 900)
-		{
-			downFaces1(vertices);
-			++i1;
-		}
-		if(i1>900 && i1 < 1800)
-		{
-			downFaces2(vertices);
-			++i1;
-		}
-		vector<Point> verticesCopy(vertices);
-		rotateVerticesAroundY(verticesCopy, i / 10.0);
-		drawWireFrame(screen, createCubeEdges(verticesCopy), 0, 255, 0);
+		cube.draw();
 		SDL_Flip(screen);
-		i++;
-		if (i == 3600)
-		{
-			i = 0;
-		}
-		
 	}
 	SDL_FreeSurface(screen);
 	SDL_Quit();
 	return 0;
 }
 
-void rotatePointAroundX(Point& point, const Point& basePoint, double angle)
+Mesh::Mesh(SDL_Surface* screen) : screen{ screen }, i{ 0 }, i1{ 0 }, edges(19), vertices(14), verticesCopy(14)
+{
+	double R = 100;
+	vertices[0] = Point(-R, -R, R); //0
+	vertices[1] = Point(-R, -R, -R); // 1
+	vertices[2] = Point(-R, -R, -R); // 2
+	vertices[3] = Point(R, -R, -R); // 3
+	vertices[4] = Point(R, -R, -R); // 4
+	vertices[5] = Point(-R, -R, -R); // 5
+	vertices[6] = Point(-R, -R, R); // 6
+	vertices[7] = Point(R, -R, R); // 7
+	vertices[8] = Point(R, -R, R); // 8
+	vertices[9] = Point(-R, -R, R); // 9
+	vertices[10] = Point(-R, R, R); // 10
+	vertices[11] = Point(-R, R, -R); // 11
+	vertices[12] = Point(R, R, -R); // 12
+	vertices[13] = Point(R, R, R); // 13
+}
+void Mesh::draw()
+{
+	downFaces();
+	verticesCopy = vertices;
+	rotateVerticesAroundY(i / 10.0);
+	createCubeEdges();
+	drawWireFrame(0, 255, 0);
+	if (++i == 3600)
+	{
+		i = 0;
+	}
+}
+
+void Mesh::rotatePointAroundX(Point& point, const Point& basePoint, double angle)
 {
 	// rotation of a point (point) around the base point (basePoint) along the axis X
 	angle = angle * PI / 180;
@@ -107,7 +124,8 @@ void rotatePointAroundX(Point& point, const Point& basePoint, double angle)
 	point.z = point.y * sin_ + point.z * cos_ + basePoint.z;
 	point.y = temp + basePoint.y;
 }
-void rotatePointAroundY(Point& point, const Point& basePoint, double angle)
+
+void Mesh::rotatePointAroundY(Point& point, const Point& basePoint, double angle)
 {
 	// rotation of a point (point) around the base point (basePoint) along the axis Y
 	angle = angle * PI / 180;
@@ -120,7 +138,7 @@ void rotatePointAroundY(Point& point, const Point& basePoint, double angle)
 	point.x = temp + basePoint.x;
 }
 
-void rotatePointAroundZ(Point& point, const Point& basePoint, double angle)
+void Mesh::rotatePointAroundZ(Point& point, const Point& basePoint, double angle)
 {
 	// rotation of a point (point) around the base point (basePoint) along the axis Z
 	angle = angle * PI / 180;
@@ -133,28 +151,32 @@ void rotatePointAroundZ(Point& point, const Point& basePoint, double angle)
 	point.x = temp + basePoint.x;
 }
 
-void downFaces1(vector<Point>& vertexes)
+void Mesh::downFaces()
 {
 	double angle = 0.1;
-	rotatePointAroundZ(vertexes[0], vertexes[10], -angle);
-	rotatePointAroundZ(vertexes[1], vertexes[11], -angle);
-	rotatePointAroundZ(vertexes[5], vertexes[4], angle);
-	rotatePointAroundZ(vertexes[6], vertexes[7], angle);
-	rotatePointAroundX(vertexes[2], vertexes[11], angle);
-	rotatePointAroundX(vertexes[3], vertexes[12], angle);
-	rotatePointAroundX(vertexes[9], vertexes[10], -angle);
-	rotatePointAroundX(vertexes[8], vertexes[13], -angle);
-}
-void downFaces2(vector<Point>& vertexes)
-{
-	double angle = 0.1;
-	rotatePointAroundZ(vertexes[5], vertexes[12], angle);
-	rotatePointAroundZ(vertexes[6], vertexes[13], angle);
-	rotatePointAroundZ(vertexes[4], vertexes[12], angle);
-	rotatePointAroundZ(vertexes[7], vertexes[13], angle);
+	if (i1 <= 900)
+	{
+		rotatePointAroundZ(vertices[0], vertices[10], -angle);
+		rotatePointAroundZ(vertices[1], vertices[11], -angle);
+		rotatePointAroundZ(vertices[5], vertices[4], angle);
+		rotatePointAroundZ(vertices[6], vertices[7], angle);
+		rotatePointAroundX(vertices[2], vertices[11], angle);
+		rotatePointAroundX(vertices[3], vertices[12], angle);
+		rotatePointAroundX(vertices[9], vertices[10], -angle);
+		rotatePointAroundX(vertices[8], vertices[13], -angle);
+		++i1;
+	}
+	if (i1 > 900 && i1 < 1800)
+	{
+		rotatePointAroundZ(vertices[5], vertices[12], angle);
+		rotatePointAroundZ(vertices[6], vertices[13], angle);
+		rotatePointAroundZ(vertices[4], vertices[12], angle);
+		rotatePointAroundZ(vertices[7], vertices[13], angle);
+		++i1;
+	}
 }
 
-void drawWireFrame(SDL_Surface* screen, const vector<Edge>& edges, Uint8 red, Uint8 green, Uint8 blue)
+void Mesh::drawWireFrame(Uint8 red, Uint8 green, Uint8 blue)
 {
 	double distance = 900;
 	for (int i = 0; i < edges.size(); ++i)
@@ -167,52 +189,70 @@ void drawWireFrame(SDL_Surface* screen, const vector<Edge>& edges, Uint8 red, Ui
 	}
 }
 
-void createCubeVertices(vector<Point>& vertexes)
+void Mesh::createCubeEdges()
 {
-	double R = 100;
-	vertexes.push_back({ -R, -R, R }); //0
-	vertexes.push_back({ -R, -R, -R }); // 1
-	vertexes.push_back({ -R, -R, -R }); // 2
-	vertexes.push_back({ R, -R, -R }); // 3
-	vertexes.push_back({ R, -R, -R }); // 4
-	vertexes.push_back({ -R, -R, -R }); // 5
-	vertexes.push_back({ -R, -R, R }); // 6
-	vertexes.push_back({ R, -R, R }); // 7
-	vertexes.push_back({ R, -R, R }); // 8
-	vertexes.push_back({ -R, -R, R }); // 9
-	vertexes.push_back({ -R, R, R }); // 10
-	vertexes.push_back({ -R, R, -R }); // 11
-	vertexes.push_back({ R, R, -R }); // 12
-	vertexes.push_back({ R, R, R }); // 13
-	vertexes.shrink_to_fit();
+	edges[0] = Edge(verticesCopy[0], verticesCopy[1]);
+	edges[1] = Edge(verticesCopy[1], verticesCopy[11]);
+	edges[2] = Edge(verticesCopy[11], verticesCopy[2]);
+	edges[3] = Edge(verticesCopy[2], verticesCopy[3]);
+	edges[4] = Edge(verticesCopy[3], verticesCopy[12]);
+	edges[5] = Edge(verticesCopy[12], verticesCopy[4]);
+	edges[6] = Edge(verticesCopy[4], verticesCopy[5]);
+	edges[7] = Edge(verticesCopy[5], verticesCopy[6]);
+	edges[8] = Edge(verticesCopy[6], verticesCopy[7]);
+	edges[9] = Edge(verticesCopy[4], verticesCopy[7]);
+	edges[10] = Edge(verticesCopy[7], verticesCopy[13]);
+	edges[11] = Edge(verticesCopy[13], verticesCopy[8]);
+	edges[12] = Edge(verticesCopy[8], verticesCopy[9]);
+	edges[13] = Edge(verticesCopy[9], verticesCopy[10]);
+	edges[14] = Edge(verticesCopy[10], verticesCopy[0]);
+	edges[15] = Edge(verticesCopy[10], verticesCopy[11]);
+	edges[16] = Edge(verticesCopy[11], verticesCopy[12]);
+	edges[17] = Edge(verticesCopy[12], verticesCopy[13]);
+	edges[18] = Edge(verticesCopy[13], verticesCopy[10]);
 }
 
-vector<Edge> createCubeEdges(const vector<Point>& vertexes)
+void Mesh::rotateVerticesAroundX(double angle)
 {
-	vector<Edge> cube;
-	cube.push_back({ vertexes[0], vertexes[1] });
-	cube.push_back({ vertexes[1], vertexes[11] });
-	cube.push_back({ vertexes[11], vertexes[2] });
-	cube.push_back({ vertexes[2], vertexes[3] });
-	cube.push_back({ vertexes[3], vertexes[12] });
-	cube.push_back({ vertexes[12], vertexes[4] });
-	cube.push_back({ vertexes[4], vertexes[5] });
-	cube.push_back({ vertexes[5], vertexes[6] });
-	cube.push_back({ vertexes[6], vertexes[7] });
-	cube.push_back({ vertexes[4], vertexes[7] });
-	cube.push_back({ vertexes[7], vertexes[13] });
-	cube.push_back({ vertexes[13], vertexes[8] });
-	cube.push_back({ vertexes[8], vertexes[9] });
-	cube.push_back({ vertexes[9], vertexes[10] });
-	cube.push_back({ vertexes[10], vertexes[0] });
-	cube.push_back({ vertexes[10], vertexes[11] });
-	cube.push_back({ vertexes[11], vertexes[12] });
-	cube.push_back({ vertexes[12], vertexes[13] });
-	cube.push_back({ vertexes[13], vertexes[10] });
-	cube.shrink_to_fit(); 
-	return cube; // return vector of edges
+	// Rotation of vertices around the axis X
+	angle = angle * PI / 180;
+	double cos_ = cos(angle);
+	double sin_ = sin(angle);
+	for (size_t i = 0; i < vertices.size(); ++i)
+	{
+		double temp = vertices[i].y * cos_ - vertices[i].z * sin_;
+		vertices[i].z = vertices[i].y * sin_ + vertices[i].z * cos_;
+		vertices[i].y = temp;
+	}
 }
 
+void Mesh::rotateVerticesAroundY(double angle)
+{
+	// Rotation of vertices around the axis Y
+	angle = angle * PI / 180;
+	double cos_ = cos(angle);
+	double sin_ = sin(angle);
+	for (size_t i = 0; i < verticesCopy.size(); ++i)
+	{
+		double temp = verticesCopy[i].x * cos_ - verticesCopy[i].z * sin_;
+		verticesCopy[i].z = verticesCopy[i].x * sin_ + verticesCopy[i].z * cos_;
+		verticesCopy[i].x = temp;
+	}
+}
+
+void Mesh::rotateVerticesAroundZ(double angle)
+{
+	// Rotation of vertices around the axis Z
+	angle = angle * PI / 180;
+	double cos_ = cos(angle);
+	double sin_ = sin(angle);
+	for (size_t i = 0; i < vertices.size(); ++i)
+	{
+		double temp = vertices[i].x * cos_ - vertices[i].y * sin_;
+		vertices[i].y = vertices[i].x * sin_ + vertices[i].y * cos_;
+		vertices[i].x = temp;
+	}
+}
 
 void clearScreen(SDL_Surface *screen)
 {
@@ -232,7 +272,6 @@ inline void drawPixel(SDL_Surface *screen, int x, int y, Uint8 red, Uint8 green,
 		*pixmem32 = colour;
 	}
 }
-
 
 void drawLine(SDL_Surface* screen, int x1, int y1, int x2, int y2, Uint8 red, Uint8 green, Uint8 blue)
 {
@@ -285,47 +324,5 @@ void drawCircle(SDL_Surface* screen, int x0, int y0, int radius, Uint8 red, Uint
 		++x;
 		delta += 2 * (x - y);
 		--y;
-	}
-}
-
-void rotateVerticesAroundX(vector<Point>& vertices, double angle)
-{
-	// Rotation of vertices around the axis X
-	angle = angle * PI / 180;
-	double cos_ = cos(angle);
-	double sin_ = sin(angle);
-	for (size_t i = 0; i < vertices.size(); ++i)
-	{
-		double temp = vertices[i].y * cos_ - vertices[i].z * sin_;
-		vertices[i].z = vertices[i].y * sin_ + vertices[i].z * cos_;
-		vertices[i].y = temp;
-	}
-}
-
-void rotateVerticesAroundY(vector<Point>& vertices, double angle)
-{
-	// Rotation of vertices around the axis Y
-	angle = angle * PI / 180;
-	double cos_ = cos(angle);
-	double sin_ = sin(angle);
-	for (size_t i = 0; i < vertices.size(); ++i)
-	{
-		double temp = vertices[i].x * cos_ - vertices[i].z * sin_;
-		vertices[i].z = vertices[i].x * sin_ + vertices[i].z * cos_;
-		vertices[i].x = temp;
-	}
-}
-
-void rotateVerticesAroundZ(vector<Point>& vertices, double angle)
-{
-	// Rotation of vertices around the axis Z
-	angle = angle * PI / 180;
-	double cos_ = cos(angle);
-	double sin_ = sin(angle);
-	for (size_t i = 0; i < vertices.size(); ++i)
-	{
-		double temp = vertices[i].x * cos_ - vertices[i].y * sin_;
-		vertices[i].y = vertices[i].x * sin_ + vertices[i].y * cos_;
-		vertices[i].x = temp;
 	}
 }
